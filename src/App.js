0667fdcd02d59e997ada8edf1b2fee5f1f7a50e8
fabianmontes <fabianmontes11fab@gmail.com>
2024-08-20 00:30:01 -0600

    Coneccion de Wallet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+import walletConnectFcn from "./components/hedera/walletConnect.js"
 
 function App() {
+    const [walletData, setWalletData] = useState();
+	const [account, setAccount] = useState();
+	const [network, setNetwork] = useState();
+
+    async function connectWallet() {
+		if (account !== undefined) {
+			// setConnectTextSt(`🔌 Account ${account} already connected ⚡ ✅`);
+		} else {
+			const wData = await walletConnectFcn();
+
+			let newAccount = wData[0];
+			let newNetwork = wData[2];
+			if (newAccount !== undefined) {
+				// setConnectTextSt(`🔌 Account ${newAccount} connected ⚡ ✅`);
+				// setConnectLinkSt(`https://hashscan.io/${newNetwork}/account/${newAccount}`);
+
+				setWalletData(wData);
+				setAccount(newAccount);
+				setNetwork(newNetwork);
+				// setContractTextSt();
+			}
+		}
+	}
+
     return (
-        <h1>Hola mundo</h1>
+        <div>
+            <h1>Hola mundo</h1>
+            <button onClick={connectWallet}>ConectarWallet</button>
+        </div>
     );
 }
 
